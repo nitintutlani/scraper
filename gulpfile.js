2083e6ed80572a570332ef61517f9cf46ff0ff48
Nitin Tutlani <nitintutlani@yahoo.com>
2015-09-02 18:10:44 +0530

    Updated gulpfile for synchromous clean and compile before test, added del and gulp clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,40 @@
 var gulp = require('gulp');
-
+var del = require('del');
 var ts = require('gulp-typescript');
+var mocha = require('gulp-mocha');
 
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('build', function () {
+gulp.task('clean:build', function () {
+  return del([
+    tsProject.config.compilerOptions.outDir + '/*',
+	tsProject.config.compilerOptions.outDir + '/.*'
+  ]);
+});
+
+gulp.task('compile', function () {
 	var tsResult = tsProject.src()
 		.pipe(ts(tsProject));
 	return tsResult.js.pipe(gulp.dest(tsProject.config.compilerOptions.outDir));
 });
-
-var mocha = require('gulp-mocha');
 
 gulp.task('test', function () {
     return gulp.src(tsProject.config.compilerOptions.outDir + '/**/*.spec.js', {read: false})
         .pipe(mocha({reporter: 'nyan'}));
 });
 
-gulp.task('default', ['build', 'test'], function() {
-	gulp.watch(tsProject.config.files, ['build', 'test']);
+gulp.task('build:incremental', ['compile'], function() {
+	gulp.start('test');
+});
+
+gulp.task('build:clean', ['clean:build'], function() {
+	gulp.start('build:incremental');
+});
+
+gulp.task('watch', function() {
+	gulp.watch(tsProject.config.files, ['build:incremental']);
+});
+
+gulp.task('default', ['build:clean'], function() {
+	gulp.start('watch');
 });
